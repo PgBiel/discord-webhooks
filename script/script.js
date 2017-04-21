1243d50a4dade1489c977c7156f4c3a7657d98e6
Un Known <jasonqan2004@gmail.com>
2017-04-21 19:59:25 -0400

    Debounce

--- script/script.js
+++ script/script.js
@@ -1,23 +1,22 @@
 (function(fetcher) {
   if (!fetcher) alert(
     "Warning: Fetch is not available for your browser. Check if you aren't using Internet Explorer or an older version of your browser."
-    );
-  function webCheckURL(mode) {
-    var val = $("#url").val();
-    var web = $("#weburlalert");
+  );
+  var urlBox = document.getElementById("url");
+  function checkURL (update) {
+    var val = urlBox.value;
+    var valText = document.getElementById("urlValidation");
     var webhookRegex = /^(?:https?:\/\/)?(?:www\.)?(?:canary\.)?discordapp\.com\/api\/webhooks\/\d+\/[\w-+]+$/i;
     if (!val) {
-      if (mode) web.text("This is required.");
+      if (mode) valText.innerHTML = "This is required.";
       return false;
     }
     if (!webhookRegex.test(val)) {
-      if (mode) web.text("Must be a valid webhook url.");
+      if (mode) valText.innerHTML = "Must be a valid webhook URL.";
       return false;
     }
-    if (mode) web.text("");
+    if (mode) valText.innerHTML = "";
     return true;
   }
-  window.checkURL = function(mode) {
-    return webCheckURL(mode);
-  };
+  urlBox.addEventListener("input", _.debounce(checkURL, 1000));
 })(window.fetch);
