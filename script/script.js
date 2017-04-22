8f04dcf1b8e1399712ed713444541d28da2638f6
Un Known <jasonqan2004@gmail.com>
2017-04-21 20:14:54 -0400

    Let's not be dumb about this

--- script/script.js
+++ script/script.js
@@ -3,19 +3,19 @@
     "Warning: Fetch is not available for your browser. Check if you aren't using Internet Explorer or an older version of your browser."
   );
   var urlBox = document.getElementById("url");
-  function checkURL (update) {
+  function checkURL (update = true) {
     var val = urlBox.value;
     var valText = document.getElementById("urlValidation");
     var webhookRegex = /^(?:https?:\/\/)?(?:www\.)?(?:canary\.)?discordapp\.com\/api\/webhooks\/\d+\/[\w-+]+$/i;
     if (!val) {
-      if (mode) valText.innerHTML = "This is required.";
+      if (update) valText.innerHTML = "This is required.";
       return false;
     }
     if (!webhookRegex.test(val)) {
-      if (mode) valText.innerHTML = "Must be a valid webhook URL.";
+      if (update) valText.innerHTML = "Must be a valid webhook URL.";
       return false;
     }
-    if (mode) valText.innerHTML = "";
+    if (update) valText.innerHTML = "";
     return true;
   }
   urlBox.addEventListener("input", _.debounce(checkURL, 1000));
