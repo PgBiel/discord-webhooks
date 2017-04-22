e490d09188522237934cfdad9ac1497f57abb4fa
Pg Biel <pgbiel@hotmail.com.br>
2017-04-22 04:58:07 -0300

    WOOOOH IT WORK

--- script/script.js
+++ script/script.js
@@ -4,6 +4,7 @@
   );
   var urlBox = document.getElementById("url");
   var send = document.getElementById("send");
+  var tts = document.getElementById("tts");
   var embedAdd = document.getElementById("newemb");
   var embeds = $("#embeds");
   var lastEmbed = 1;
@@ -53,14 +54,20 @@
           </div>\
           <div class="inlblock" style="margin-left: 40px;">\
             <label for="authoricon">Author Icon URL</label>\
-            <input type="url" name="avatar" class="inlblock" style="width: 20em;"/>\
+            <input type="url" name="authoricon" class="inlblock" style="width: 20em;"/>\
           </div>\
+          <br/><br/>\
+          <label for="authorurl">Author Clickable URL</label>\
+          <input type="url" name="authorurl" style="width: 20em;"/>\
           <br/><br/>\
           <label for="title">Title</label>\
           <input type="text" name="title" maxlength="256" style="width: 15em;"/>\
           <br/><br/>\
           <label for="content">Content</label>\
-          <textarea class="autoExpand" id="text" name="text" data-min-rows="1" rows="1" cols="50" maxlength="2048"></textarea>\
+          <textarea class="autoExpand" name="content" data-min-rows="1" rows="1" cols="50" maxlength="2048"></textarea>\
+          <br/><br/>\
+          <label for="sidebar">Sidebar Color</label>\
+          <input type="text" name="sidebar" placeholder="#123ABC" maxlength="7"></input>\
           <br/><br/>\
           <div class="inlblock">\
             <label for="footer">Footer</label>\
@@ -73,5 +80,111 @@
         </div>');
     autosize($('div[data-embed="' + nowEmbed + '"]>textarea'));
     $('div[data-embed="' + nowEmbed + '"]>h4>span.remove').on("click", removeEmb(nowEmbed));
+  });
+  send.addEventListener("click", function() {
+    var url = urlBox.value;
+    if (!checkURL(false)) return alert("Invalid webhook url!");
+    if (!fetcher) return alert(
+      "Fetch is not available for your browser. Check if you aren't using Internet Explorer or an older version of your browser."
+    );
+    var nick = $("#nick").val();
+    if (nick.length > 32) return alert("Please ensure the webhook nickname does not have more than 32 characters.");
+
+    var avatar = $("#avatar").val();
+
+    var content = $("#text").val() || "\u200B";
+    if (content.length > 2000) return alert("Please ensure the message content does not have more than 2000 characters.");
+
+    var tts = $("#tts").is(":checked");
+    var embedz = [];
+    var returnable = "";
+    embeds.children().each(function(index, child) {
+      var embedToAdd = {
+        type: "rich"
+      };
+      var embedNum = index + 1;
+      var title = child.children('input[name="title"]').val();
+      var shouldreturn = false;
+      if (title.length > 256) {
+        returnable += "- Please ensure the title at embed " + embedNum + " does not have more than 256 characters.\n";
+        shouldreturn = true;
+      }
+
+      var author = child.children('input[name="author"]').val();
+      if (author.length > 256) {
+        returnable += "- Please ensure the author at embed " + embedNum + " does not have more than 256 characters.\n";
+        shouldreturn = true;
+      }
+
+      var authorIcon = child.children('input[name="authorIcon"]').val();
+      var authorUrl = child.children('input[name="authorUrl"]').val();
+
+      var desc = child.children('textarea[name="content"]').val();
+      if (desc.length > 256) {
+        returnable += "- Please ensure the content at embed " + embedNum + " does not have more than 2048 characters.\n";
+        shouldreturn = true;
+      }
+
+      var sidebar = child.children('input[name="sidebar"]').val();
+      var sidebarvalid = true;
+      if (sidebar && !(/^(?:#?[\dA-F]{3}|#?[\dA-F]{6})$/i.test(sidebar))) {
+        returnable += "- Please ensure the sidebar color at embed " + embedNum + " has valid hex code color.\n";
+        shouldreturn = true;
+        sidebarvalid = false;
+      }
+      if (/^#?[\dA-F]{3}$/i.test(sidebar)) {
+        sidebar = sidebar.match(/^#?([\dA-F]{3})$/i)[1].repeat(2);
+      }
+      if (sidebarvalid && sidebar) {
+        sidebar = parseInt(sidebar.match(/^#?([\dA-F]{6})$/i), 16);
+      }
+
+      var footer = child.children('input[name="footer"]').val();
+      if (footer.length > 256) {
+        returnable += "- Please ensure the footer at embed " + embedNum + " does not have more than 256 characters.\n";
+        shouldreturn = true;
+      }
+      var footerIcon = child.children('input[name="footericon"]').val();
+
+      if (shouldreturn) return;
+      if (title) embedToAdd.title = title;
+      if (author) embedToAdd.author = {
+        name: author
+      };
+      if (authorIcon) {
+        if (!embedToAdd.author) embedToAdd.author = { name: "\u200B" };
+        embedToAdd.author.icon_url = authorIcon;
+      }
+      if (authorUrl && embedToAdd.author) embedToAdd.author.url = authorUrl;
+      if (desc) embedToAdd.description = desc;
+      if (sidebar) embedToAdd.color = sidebar;
+      if (footer) embedToAdd.footer = {
+        text: footer
+      };
+      if (footerIcon) {
+        if (!embedToAdd.footer) embedToAdd.footer = { text: "\u200B" };
+        embedToAdd.footer.icon_url = footerIcon;
+      }
+      if (
+        !embedToAdd.description && !embedToAdd.footer && !embedToAdd.author && !embedToAdd.title
+      ) embedToAdd.description = "\u200B";
+      embedz.push(embedToAdd);
+    });
+    if (returnable) return alert(returnable.replace(/\s+$/, ""));
+    var obj = {
+      content,
+      tts: !!tts,
+      embeds: embedz
+    };
+    if (nick) obj.username = nick;
+    if (avatar) obj.avatar_url = avatar;
+    fetcher(url.startsWith("http") ? url : ("https://" + url), {
+      method: "POST",
+      headers: new Headers({"Content-Type": "application/json"}),
+      body: JSON.stringify(obj),
+      referrerPolicy: "no-referrer"
+    }).then(function(response) {
+      if (!response.ok) return alert("Discord gave status of " + response.status);
+    });
   });
 })(window.fetch);
